56e6c03d12c07629e7fb2ba4cd99b632c06b4bb1
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-05-25 01:14:53 -0400

    decoupled page image tests a little

--- cypress/integration/starlight/cypress.config.js
+++ cypress/integration/starlight/cypress.config.js
@@ -2,6 +2,11 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('before:browser:launch',(browser, launchOptions) => {
+        if (browser.name === 'electron') {
+          launchOptions.preferences
+        }
+      })
     },
   },
 };
--- cypress/integration/starlight/websiteImages.spec.js
+++ cypress/integration/starlight/websiteImages.spec.js
@@ -1,53 +1,57 @@
 describe('Checking for broken images on Starlight Music pages', () => {
-    const pagesToCheck = [
+    const mainPagesToCheck = [
       '/',
-      '/band/onyx-nyc-wedding-band',
-      '/band/rock-and-soul-nyc-premiere-wedding-band',
-      '/band/starlight-orchestra-corporate-events-and-nyc-weddings',
-      '/band/string-performers-eve-nyc-wedding-corporate-musicians',
-      '/cities/aspen-wedding-bands',
       '/our-talent',
-    //   '/about-us',
-    //   '/band/acoustic-guitar-ensembles',
-    //   '/band/dj-kristaval-nyc-celebrity-disc-jockey',
-    //   '/band/fleur-seule',
-    //   '/band/latin---acoustic-guitar',
-    //   '/band/star-power-nyc-wedding-band',
-    //   '/band/the-blake-band-nyc-wedding-band',
-    //   '/band/the-starlight-experience',
-    //   '/band/white-light-nyc-wedding-band',
-    //   '/celebrities',
-    //   '/cities/boca-raton-wedding-bands',
-    //   '/cities/boston-wedding-bands',
-    //   '/cities/chicago-wedding-bands',
-    //   '/cities/dallas-wedding-bands',
-    //   '/cities/denver-wedding-bands',
-    //   '/cities/detroit-wedding-bands',
-    //   '/cities/houston-wedding-bands',
-    //   '/cities/las-vegas-wedding-bands',
-    //   '/cities/los-angeles-wedding-bands',
-    //   '/cities/maine-wedding-bands',
-    //   '/cities/mexico-wedding-bands',
-    //   '/cities/miami-wedding-bands',
-    //   '/cities/newport-ri-wedding-bands',
-    //   '/cities/palm-beach-wedding-bands',
-    //   '/cities/philadelphia-wedding-bands',
-    //   '/cities/pittsburgh-wedding-bands',
-    //   '/cities/san-francisco-wedding-bands',
-    //   '/cities/st-louis-wedding-bands',
-    //   '/cities/the-hamptons-wedding-bands',
-    //   '/cities/vermont-wedding-bands',
-    //   '/cities/washington-dc-wedding-bands',
-    //   '/contact-us',
-    //   '/credits',
-    //   '/faq',
-    //   '/meet-the-team',
-    //   '/privacy-polices',
-    //   '/terms-and-conditions'
+      '/about-us',
+      '/celebrities',
+      '/contact-us',
+      '/credits',
+      '/faq',
+      '/meet-the-team',
+      '/privacy-polices',
+      '/terms-and-conditions'
     ];
+
+    const bandPagesToCheck = [
+      '/band/acoustic-guitar-ensembles',
+      '/band/dj-kristaval-nyc-celebrity-disc-jockey',
+      '/band/fleur-seule',
+      '/band/latin---acoustic-guitar',
+      '/band/star-power-nyc-wedding-band',
+      '/band/the-blake-band-nyc-wedding-band',
+      '/band/the-starlight-experience',
+      '/band/white-light-nyc-wedding-band'
+    ]
+
+    const cityPagesToCheck = [
+      '/cities/aspen-wedding-bands',
+      '/cities/boca-raton-wedding-bands',
+      '/cities/boston-wedding-bands',
+      '/cities/chicago-wedding-bands',
+      '/cities/dallas-wedding-bands',
+      '/cities/denver-wedding-bands',
+      '/cities/detroit-wedding-bands',
+      '/cities/houston-wedding-bands',
+      '/cities/las-vegas-wedding-bands',
+      '/cities/los-angeles-wedding-bands',
+      '/cities/maine-wedding-bands',
+      '/cities/mexico-wedding-bands',
+      '/cities/miami-wedding-bands',
+      '/cities/newport-ri-wedding-bands',
+      '/cities/palm-beach-wedding-bands',
+      '/cities/philadelphia-wedding-bands',
+      '/cities/pittsburgh-wedding-bands',
+      '/cities/san-francisco-wedding-bands',
+      '/cities/st-louis-wedding-bands',
+      '/cities/the-hamptons-wedding-bands',
+      '/cities/vermont-wedding-bands',
+      '/cities/washington-dc-wedding-bands',
+    ];
+
+
   
-    pagesToCheck.forEach((page) => {
-      context(`On page ${page}`, () => {
+    mainPagesToCheck.forEach((page) => {
+      context(`On main page ${page}`, () => {
         it('should not have any broken images', () => {
           cy.visit(`https://www.starlightmusic.com${page}`);
           cy.get('img').each((img) => {
@@ -58,4 +62,31 @@ describe('Checking for broken images on Starlight Music pages', () => {
         });
       });
     });
+
+    bandPagesToCheck.forEach((page) => {
+      context(`On band page ${page}`, () => {
+        it('should not have any broken images', () => {
+          cy.visit(`https://www.starlightmusic.com${page}`);
+          cy.get('img').each((img) => {
+            cy.request(img.prop('src'))
+              .its('status')
+              .should('eq', 200);
+          });
+        });
+      });
+    });
+
+    cityPagesToCheck.forEach((page) => {
+      context(`On city page ${page}`, () => {
+        it('should not have any broken images', () => {
+          cy.visit(`https://www.starlightmusic.com${page}`);
+          cy.get('img').each((img) => {
+            cy.request(img.prop('src'))
+              .its('status')
+              .should('eq', 200);
+          });
+        });
+      });
+    });
+
   });
