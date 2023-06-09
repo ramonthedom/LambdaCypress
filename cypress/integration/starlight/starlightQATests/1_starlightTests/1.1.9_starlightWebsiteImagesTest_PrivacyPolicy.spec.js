807df8690668dd6d34b6ece2e0f1d2ad437471b0
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-06-08 23:05:35 -0400

    seperated page image tests

---
+++ cypress/integration/starlight/starlightQATests/1_starlightTests/1.1.9_starlightWebsiteImagesTest_PrivacyPolicy.spec.js
@@ -0,0 +1,22 @@
+describe('Checking for broken images on Starlight Music pages', () => {
+
+  const mainPagesToCheck = [
+    '/privacy-polices'
+  ];
+
+  // Define a function to check for broken images.
+  function checkForBrokenImages(page) {
+    cy.visit(`https://www.starlightmusic.com${page}`);
+    cy.get('img').each((img) => {
+      cy.request(img.prop('src'))
+        .its('status')
+        .should('eq', 200);
+    });
+  }
+
+  // Check for broken images on the main pages.
+  it('should be check all images on main pages', () => {
+    mainPagesToCheck.forEach(checkForBrokenImages);
+  }); 
+
+});
