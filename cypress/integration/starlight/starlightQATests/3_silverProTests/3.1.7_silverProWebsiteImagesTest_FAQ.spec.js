6a239db4b91fed6a0452c69cc756cdbb2eec5462
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-06-23 13:43:17 -0400

    updated for contact us page url change & faw modal click through

--- cypress/integration/starlight/starlightQATests/3_silverProTests/3.1.7_silverProWebsiteImagesTest_FAQ.spec.js
+++ cypress/integration/starlight/starlightQATests/3_silverProTests/3.1.7_silverProWebsiteImagesTest_FAQ.spec.js
@@ -6,6 +6,11 @@ describe('Checking for broken images on Silver Pro pages', function () {
   // Define a function to check for broken images.
   function checkForBrokenImages (page) {
     cy.visit(`https://new.silverproentertainment.com${page}`)
+
+    cy.get('.modal-content').should('be.visible')
+    cy.get('.modal-close').click()
+    cy.get('.modal-content').should('not.be.visible')
+    
     cy.get('img').each((img) => {
       cy.request(img.prop('src'))
         .its('status')
