87058de87ca9b7a178a776a55c16af13e19a7d24
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-08-04 13:49:34 -0400

    added Safari Catalina and Mojave to tests

--- cypress/integration/starlight/starlightQATests1_old/1_starlightTests/1.1_starlightWebsiteImagesTests.js
+++ cypress/integration/starlight/starlightQATests1_old/1_starlightTests/1.1_starlightWebsiteImagesTests.js
@@ -1,7 +1,15 @@
 describe('Checking for broken images on Starlight Music pages', function () {
-  const mainPagesToCheck = [
-    '/',
-  ]
+
+  const mainPage = '/'
+  const ourTalentPage = '/our-talent'
+  const aboutUsPage = '/about-us'
+  const celebritiesPage = '/celebrities'
+  const contactUsPage = '/contact-us'
+  const creditsPage = '/credits'
+  const faqPage = '/faq'
+  const meetTheTeamPage = '/meet-the-team'
+  const privacyPoliciesPage = '/privacy-polices'
+  const termsAndConditionsPage = '/terms-and-conditions'
 
   // Define a function to check for broken images.
   function checkForBrokenImages (page) {
@@ -14,7 +22,44 @@ describe('Checking for broken images on Starlight Music pages', function () {
   }
 
   // Check for broken images on the main pages.
-  it('should be check all images on main pages', function () {
-    mainPagesToCheck.forEach(checkForBrokenImages)
+  it('should be check all images on Main page', function () {
+    checkForBrokenImages(mainPage)
   })
+
+  it('should be check all images on Talent page', function () {
+    checkForBrokenImages(ourTalentPage)
+  })
+
+  it('should be check all images on About Us page', function () {
+    checkForBrokenImages(aboutUsPage)
+  })
+
+  it('should be check all images on Celebrities page', function () {
+    checkForBrokenImages(celebritiesPage)
+  })
+
+  it('should be check all images on ContactUs page', function () {
+    checkForBrokenImages(contactUsPage)
+  })
+
+  it('should be check all images on Credits page', function () {
+    checkForBrokenImages(creditsPage)
+  })
+
+  it('should be check all images on FAQ page', function () {
+    checkForBrokenImages(faqPage)
+  })
+
+  it('should be check all images on Mee The Team page', function () {
+    checkForBrokenImages(meetTheTeamPage)
+  })
+
+  it('should be check all images on Privacy Policy page', function () {
+    checkForBrokenImages(privacyPoliciesPage)
+  })
+
+  it('should be check all images on Terms & Conditions page', function () {
+    checkForBrokenImages(termsAndConditionsPage)
+  })
+
 })
