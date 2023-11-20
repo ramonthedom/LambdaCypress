ad92243416975a45e11d23fa124b19ea7480e5cb
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-11-20 12:10:08 -0500

    update .mb-3 wait times for utilities file

--- cypress/integration/starlight/starlightQATests/3_silverProTests/utilities.js
+++ cypress/integration/starlight/starlightQATests/3_silverProTests/utilities.js
@@ -559,6 +559,7 @@
 
     export function checkFinalsSpecialSongsSection() {
         cy.contains('.finals_sidebar_title', 'Special Songs').should('exist').click().then(() => {
+            cy.wait(500);
             cy.get('.finals_main_container').find('.mb-3').should('have.length.greaterThan', 1); // .finals_main_container should contain more than 1 elemtent .mb-3
             cy.contains('.finals_main_add_song_card', 'Click to add a song').should('exist');
         });
@@ -592,6 +593,7 @@
 
     export function checkFinalsBlessingToastSection() {
         cy.contains('.finals_sidebar_title', 'Blessing/Toast').should('exist').click().then(() => {
+            cy.wait(500);
             cy.get('.finals_main_container').find('.mb-3').should('have.length.greaterThan', 1); // .finals_main_container should contain more than 1 elemtent .mb-3
             cy.contains('.finals_main_add_song_card', 'Click to add a participant').should('exist');
         });
