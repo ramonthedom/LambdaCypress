f2cd53926ba014322fbcc957f0be3d1d83350729
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2023-12-04 10:38:24 -0500

    add restart services if contractPreview fails

--- cypress/integration/starlight/starlightQATests/1_starlightTests/1.6_starPreviewContractCheck.spec.js
+++ cypress/integration/starlight/starlightQATests/1_starlightTests/1.6_starPreviewContractCheck.spec.js
@@ -1,16 +1,36 @@
 describe('API Contract Preview Test', () => {
   it('should confirm that the contract preview page does not return a 500 error', () => {
-
-    const testEventId = "63560fc8e27cdf19b676de1c" // Steve Rogers OX3/21/2029 Gotham Hall
-    const contractUrl = 'https://star-api.starlightmusic.com/events/api/v1/contract/preview/' + testEventId
-    // https://star-api.starlightmusic.com/events/api/v1/contract/preview/63560fc8e27cdf19b676de1c
+    const testEventId = "63560fc8e27cdf19b676de1c"; // Steve Rogers OX3/21/2029 Gotham Hall
+    const contractUrl = 'https://star-api.starlightmusic.com/events/api/v1/contract/preview/' + testEventId;
 
     cy.request({      
       url: contractUrl,
-      failOnStatusCode: false // This will prevent Cypress from failing the test on server errors
+      failOnStatusCode: false
     }).then((response) => {
-      expect(response.status).to.not.equal(500);
-      expect(response.status).to.equal(200);
+      if (response.status === 500) {
+        // If a 500 error is encountered, trigger the Lambda function via API Gateway
+        const lambdaTriggerUrl = 'https://8ze16mvrz1.execute-api.us-east-1.amazonaws.com/prod/resetContractPreviewServices'; // Replace with your API Gateway URL
+        const lambdaPayload = {
+          parameter: 'starlight' // Replace with the appropriate parameter if necessary
+        };
+
+        cy.request({
+          method: 'POST',
+          url: lambdaTriggerUrl,
+          body: lambdaPayload,
+          failOnStatusCode: false
+        }).then(lambdaResponse => {
+          // Handle the response from the Lambda function if needed
+          expect(lambdaResponse.status).to.equal(200);
+          cy.log('Lambda function ContractPreviewErrorSvcReset triggered successfully.');
+        });
+
+        // Optionally, you can fail the test after triggering the Lambda function
+        expect(response.status).to.not.equal(500);
+      } else {
+        // Original assertion for non-500 status
+        expect(response.status).to.equal(200);
+      }
     });
   });
 });
--- cypress/integration/starlight/starlightQATests/3_silverProTests/3.6_sproPreviewContractCheck.spec.js
+++ cypress/integration/starlight/starlightQATests/3_silverProTests/3.6_sproPreviewContractCheck.spec.js
@@ -1,16 +1,37 @@
 describe('API Contract Preview Test', () => {
   it('should confirm that the contract preview page does not return a 500 error', () => {
 
-    const testEventId = "64a4f8508ee20fd52821ff34" // Ramon de Bruyn TRS1/03/20291 Liberty Plaza
-    const contractUrl = 'https://star-api.starlightmusic.com/silproevent/api/v1/contract/preview/' + testEventId
-    // https://star-api.starlightmusic.com/silproevent/api/v1/contract/preview/64a4f8508ee20fd52821ff34 
+    const testEventId = "64a4f8508ee20fd52821ff34"; // Ramon de Bruyn TRS1/03/20291 Liberty Plaza
+    const contractUrl = 'https://star-api.starlightmusic.com/silproevent/api/v1/contract/preview/' + testEventId;
 
     cy.request({      
       url: contractUrl,
-      failOnStatusCode: false // This will prevent Cypress from failing the test on server errors
+      failOnStatusCode: false
     }).then((response) => {
-      expect(response.status).to.not.equal(500);
-      expect(response.status).to.equal(200);
+      if (response.status === 500) {
+        // If a 500 error is encountered, trigger the Lambda function via API Gateway
+        const lambdaTriggerUrl = 'https://8ze16mvrz1.execute-api.us-east-1.amazonaws.com/prod/resetContractPreviewServices'; // Replace with your API Gateway URL
+        const lambdaPayload = {
+          parameter: 'silverpro' // Replace with the appropriate parameter if necessary
+        };
+
+        cy.request({
+          method: 'POST',
+          url: lambdaTriggerUrl,
+          body: lambdaPayload,
+          failOnStatusCode: false
+        }).then(lambdaResponse => {
+          // Handle the response from the Lambda function if needed
+          expect(lambdaResponse.status).to.equal(200);
+          cy.log('Lambda function ContractPreviewErrorSvcReset triggered successfully.');
+        });
+
+        // Optionally, you can fail the test after triggering the Lambda function
+        expect(response.status).to.not.equal(500);
+      } else {
+        // Original assertion for non-500 status
+        expect(response.status).to.equal(200);
+      }
     });
   });
 });
