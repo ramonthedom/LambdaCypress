3b0231aec803f27d3260c9e23412fb0d62e48c9b
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2024-01-04 14:19:16 -0500

    increased waiing time in div > Sorting > Event Details from 500 to 1000

--- cypress/integration/starlight/starlightQATests/1_starlightTests/1.3_starbridgeSectionDisplayChecks.spec.js
+++ cypress/integration/starlight/starlightQATests/1_starlightTests/1.3_starbridgeSectionDisplayChecks.spec.js
@@ -242,7 +242,7 @@ describe('Check User Starbridge Sections', function () {
 
     /// 2.4. Subsection - Gig Info
     cy.contains('div', 'Gig Info').should('exist').click();
-    cy.contains('div', 'Sorting').should('exist').click().wait(500).then(() => {
+    cy.contains('div', 'Sorting').should('exist').click().wait(1000).then(() => {
       cy.contains('span', 'Event Details').should('exist');
       cy.contains('div', 'Styling').should('exist').click();
       cy.contains('h5', 'Segments').should('exist');
