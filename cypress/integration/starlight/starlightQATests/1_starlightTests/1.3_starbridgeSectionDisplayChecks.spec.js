4501dec92957fdaa8f9e272033593652414c8d9f
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2024-01-05 09:42:23 -0500

    update Leads test a to span

--- cypress/integration/starlight/starlightQATests/1_starlightTests/1.3_starbridgeSectionDisplayChecks.spec.js
+++ cypress/integration/starlight/starlightQATests/1_starlightTests/1.3_starbridgeSectionDisplayChecks.spec.js
@@ -258,7 +258,7 @@ describe('Check User Starbridge Sections', function () {
     visitStarbridge()
 
     // 3. Leads
-    cy.contains('a', 'Leads').should('exist').click();
+    cy.contains('span', 'Leads').should('exist').click();
     // cy.get('a > .leads-table-css-container').should('exist');
     cy.contains('a', 'Inquiry').should('exist');
     cy.contains('a', 'Showcase').should('exist');
