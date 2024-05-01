3114328f4bcacda3522fa93b5be5009b8b894d57
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2024-05-01 11:24:16 -0400

    silverops changed wedding event to check

--- cypress/integration/starlight/starlightQATests/3_silverProTests/3.3_silverOpsSectionDisplayChecks.spec.js
+++ cypress/integration/starlight/starlightQATests/3_silverProTests/3.3_silverOpsSectionDisplayChecks.spec.js
@@ -619,8 +619,10 @@ describe('Check User SilverOps Sections', function () {
 
       // check a wedding
       // cy.contains('a', 'Wedding').should('exist').click().then(() => {
-      // const sampleLiveWeddingEventId = "6479d620a6c515bcc0e8500c" //TRS 4/27/2024 
-      cy.get('a[href="/event/6479d620a6c515bcc0e8500c"]').first().click().wait(1500).then(() => {
+      // const sampleLiveWeddingEventId = "6479d620a6c515bcc0e8500c" //TRS 4/27/2024
+      // const sampleLiveWeddingEventId = "6479d621a6c515bcc0e851dc" //TRS 9/01/2024
+
+      cy.get('a[href="/event/6479d621a6c515bcc0e851dc"]').first().click().wait(1500).then(() => {
         cy.wait(3000);
 
         // 4.1.1.1 Basic Info
