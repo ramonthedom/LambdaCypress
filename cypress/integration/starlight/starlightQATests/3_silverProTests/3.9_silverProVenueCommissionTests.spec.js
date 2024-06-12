ffca9611e8d55874933e665baabeb224746afb2d
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2024-06-12 14:14:01 -0400

    added venue commision checks for SilverPro APIs

---
+++ cypress/integration/starlight/starlightQATests/3_silverProTests/3.9_silverProVenueCommissionTests.spec.js
@@ -0,0 +1,103 @@
+describe('Testing the venue commissions for SilverPro Band Service', function () {
+  // it('Blue Hill at Stone Barns commission should be greater than 0%', function () {
+  // //   const url = baseUrl + bridgelogin
+  //   const url = 'https://star-api.starlightmusic.com/starlightband/api/v1/band/619ea78bd9b171af1ce6e328/configurations/wedding/2029-10-11?latitude=41.1042276&longitude=-73.8285566&event_place=630%2BBedford%2BRd%2BTarrytown%2BNY%2B10591%2BUSA'
+
+  //   cy.request('GET', url)
+  //     .then((response) => {
+  //         expect(response.status).to.eq(200)
+
+  //         cy.log('Response: ' + JSON.stringify(response))
+  //         expect(response.body.commission.totalCommission).to.be.greaterThan(0)
+  //         expect(response.body.commission.venue).to.be.greaterThan(0)
+  //         expect(response.body.commission.venueAdditional).to.be.greaterThan(0)
+
+  //     })
+  // })
+
+  
+  // Array of venues with their respective URLs
+  const venues = [{
+      "name": "1 Hotel Brooklyn Bridge",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747cc16605b4056e836bb2/configurations/wedding/2025-06-14?latitude=40.70228539999999&longitude=-73.9955827&event_place=60%2BFurman%2BSt%2BBrooklyn%2BNY%2B11201%2BUSA"
+    },
+    {
+      "name": "91 Horatio Street",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7389297&longitude=-74.00858199999999&event_place=91%2BHoratio%2BSt%2BNew%2BYork%2BNY%2B10014%2BUSA"
+    },
+    {
+      "name": "Blue Hill at Stone Barns",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=41.1042276&longitude=-73.8285566&event_place=630%2BBedford%2BRd%2BTarrytown%2BNY%2B10591%2BUSA"
+    },
+    {
+      "name": "Bowery Hotel",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7260114&longitude=-73.9916125&event_place=335%2BBowery%2BNew%2BYork%2BNY%2B10003%2BUSA"
+    },
+    {
+      "name": "Carnegie Hall",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7651258&longitude=-73.97992359999999&event_place=881%2B7th%2BAve%2BNew%2BYork%2BNY%2B10019%2BUSA"
+    },
+    {
+      "name": "Chelsea Pier-Lighthouse",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7477512&longitude=-74.01110179999999&event_place=The%2BLighthouse%2BPier%2B61%2BChelsea%2BPiers%2BNew%2BYork%2BNY%2B10011%2BUSA"
+    },
+    {
+      "name": "Chelsea Pier-Pier 60",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.746645&longitude=-74.01005700000002&event_place=Pier%2B60%2BChelsea%2BPiers%2BNew%2BYork%2BNY%2B10011%2BUSA"
+    },
+    {
+      "name": "Current- Chelsea Piers",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7481792&longitude=-74.0086142&event_place=62%2BChelsea%2BPiers%2BNew%2BYork%2BNY%2B10011%2BUSA"
+    },
+    {
+      "name": "Rainbow Room",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7593331&longitude=-73.9795094&event_place=30%2BRockefeller%2BPlaza%2B65th%2BFloor%2BNew%2BYork%2BNY%2B10112%2BUSA"
+    },
+    {
+      "name": "Tappan Hill",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=41.0719326&longitude=-73.8536238&event_place=81%2BHighland%2BAve%2BTarrytown%2BNY%2B10591%2BUSA"
+    },
+    {
+      "name": "The Loading Dock- Stamford, CT",
+      "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=41.0364835&longitude=-73.5533899&event_place=375%2BFairfield%2BAve%2BStamford%2BCT%2B06902%2BUSA"
+    }
+  ];
+
+  // Array of venues with no commission
+  const venuesNoCommission = [{
+    "name": "Gotham Hall",
+    "url": "https://star-api.starlightmusic.com/silproband/api/v1/band/64747c65516aa74330b8371e/configurations/wedding/2025-06-14?latitude=40.7516243&longitude=-73.987325&event_place=Gotham%2BHall%2B1356%2BBroadway%2BNew%2BYork%2BNY%2B10018%2BUSA"
+  }];
+
+  // For each venue, test that the commission is greater than 0%
+  venues.forEach(venue => {
+    it(`${venue.name} commission should be greater than 0%`, function () {
+      const url = venue.url
+
+      cy.request('GET', url)
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          // Check that the total commission, venue commission, and additional venue commission are all greater than 0
+          expect(response.body.commission.totalCommission).to.be.greaterThan(0)
+          expect(response.body.commission.venue).to.be.greaterThan(0)
+          expect(response.body.commission.venueAdditional).to.be.greaterThan(0)
+        })
+    })
+  })
+
+  // For each venue with no commission, test that the commission is equal to 0%
+  venuesNoCommission.forEach(venue => {
+    it(`Anti-check: ${venue.name} commission should be equal to 0%`, function () {
+      const url = venue.url
+
+      cy.request('GET', url)
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          // Check that the total commission, venue commission, and additional venue commission are all equal to 0
+          expect(response.body.commission.totalCommission).to.eq(0)
+          expect(response.body.commission.venue).to.eq(0)
+          expect(response.body.commission.venueAdditional).to.eq(0)
+        })
+    })
+  })
+})
